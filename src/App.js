f8ffa3f74fbf4403ede53c6a01f725551e7d9f0c
lulupang2 <lulupang@live.co.kr>
2021-09-13 00:12:07 +0900

    _

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router';
 import './App.css';
-
+import Menu from './components/menu';
+import Home from './components/home';
+import Products from './components/products/products';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg">
+        <div className="container">        
+          <Menu />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/products" component={Products} />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/products/products.jsx
@@ -0,0 +1,17 @@
+import './products.css'
+
+const Products = () => {
+    return (
+        <div className="Products">
+            <div className="긴줄">
+                ㅇㅇ
+            </div>
+            <div className="products_list_wrap">
+1
+
+            </div>
+        </div>
+    )
+}
+
+export default Products
